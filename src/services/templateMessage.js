5102933d42bac7c3da37d90f35f1304424da6415
GIA CUONG <tgiacuong@gmail.com>
2023-05-25 18:07:05 +0900

    fix

--- src/services/templateMessage.js
+++ src/services/templateMessage.js
@@ -1,79 +1,79 @@
-let sendCategoriesTemplate = () =>{
-    return {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [
-                    {
-                        "title": "Headphones",
-                        "image_url": "https://bit.ly/imageHeadphones",
-                        "subtitle": "Bose Noise Cancelling Wireless Bluetooth Headphones",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://bit.ly/webHeadphones",
-                            "webview_height_ratio": "tall",
-                        },
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": "https://bit.ly/webHeadphones",
-                                "title": "View on Website"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "Show Headphones",
-                                "payload": "SHOW_HEADPHONES"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "TV",
-                        "image_url": "https://bit.ly/imageTV",
-                        "subtitle": "Master of quality & Incredible clarity",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://bit.ly/webTelevision",
-                            "webview_height_ratio": "tall",
-                        },
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": "https://bit.ly/webTelevision",
-                                "title": "View on Website"
-                            }, {
-                                "type": "postback",
-                                "title": "Show TVs",
-                                "payload": "SHOW_TV"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "Playstation",
-                        "image_url": "https://bit.ly/imagePlaystation",
-                        "subtitle": "Incredible games & Endless entertainment",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://bit.ly/webPlaystation",
-                            "webview_height_ratio": "tall",
-                        },
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": "https://bit.ly/webPlaystation",
-                                "title": "View on Website"
-                            }, {
-                                "type": "postback",
-                                "title": "Show Playstation",
-                                "payload": "SHOW_PLAYSTATION"
-                            }
-                        ]
-                    },
-                ]
-            }
-        }
-    };
-};
+// let sendCategoriesTemplate = () =>{
+//     return {
+//         "attachment": {
+//             "type": "template",
+//             "payload": {
+//                 "template_type": "generic",
+//                 "elements": [
+//                     {
+//                         "title": "Headphones",
+//                         "image_url": "https://bit.ly/imageHeadphones",
+//                         "subtitle": "Bose Noise Cancelling Wireless Bluetooth Headphones",
+//                         "default_action": {
+//                             "type": "web_url",
+//                             "url": "https://bit.ly/webHeadphones",
+//                             "webview_height_ratio": "tall",
+//                         },
+//                         "buttons": [
+//                             {
+//                                 "type": "web_url",
+//                                 "url": "https://bit.ly/webHeadphones",
+//                                 "title": "View on Website"
+//                             },
+//                             {
+//                                 "type": "postback",
+//                                 "title": "Show Headphones",
+//                                 "payload": "SHOW_HEADPHONES"
+//                             }
+//                         ]
+//                     },
+//                     {
+//                         "title": "TV",
+//                         "image_url": "https://bit.ly/imageTV",
+//                         "subtitle": "Master of quality & Incredible clarity",
+//                         "default_action": {
+//                             "type": "web_url",
+//                             "url": "https://bit.ly/webTelevision",
+//                             "webview_height_ratio": "tall",
+//                         },
+//                         "buttons": [
+//                             {
+//                                 "type": "web_url",
+//                                 "url": "https://bit.ly/webTelevision",
+//                                 "title": "View on Website"
+//                             }, {
+//                                 "type": "postback",
+//                                 "title": "Show TVs",
+//                                 "payload": "SHOW_TV"
+//                             }
+//                         ]
+//                     },
+//                     {
+//                         "title": "Playstation",
+//                         "image_url": "https://bit.ly/imagePlaystation",
+//                         "subtitle": "Incredible games & Endless entertainment",
+//                         "default_action": {
+//                             "type": "web_url",
+//                             "url": "https://bit.ly/webPlaystation",
+//                             "webview_height_ratio": "tall",
+//                         },
+//                         "buttons": [
+//                             {
+//                                 "type": "web_url",
+//                                 "url": "https://bit.ly/webPlaystation",
+//                                 "title": "View on Website"
+//                             }, {
+//                                 "type": "postback",
+//                                 "title": "Show Playstation",
+//                                 "payload": "SHOW_PLAYSTATION"
+//                             }
+//                         ]
+//                     },
+//                 ]
+//             }
+//         }
+//     };
+// };
 
 let sendHeadphonesTemplate = () =>{
     return {
