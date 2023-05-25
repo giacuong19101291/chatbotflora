c26eb7574f401a2b33bdf61a4f346cacf688d75b
GIA CUONG <tgiacuong@gmail.com>
2023-05-25 20:33:03 +0900

    make it better

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,14 +4,13 @@ import homepageController from "../controllers/homepageController";
 let router = express.Router();
 
 let initWebRoutes = (app)=> {
-    router.get("/", homepageController.getHomePage);
-    router.get("/webhook", homepageController.getWebhook);
-    router.post("/webhook", homepageController.postWebhook);
-    router.post("/set-up-profile", homepageController.handleSetupProfile);
-    router.get("/set-up-profile", homepageController.getSetupProfilePage);
-
-    router.get("/info-order", homepageController.getInfoOrderPage);
-    router.post("/set-info-order", homepageController.setInfoOrder);
+    router.get("/", homepageController.getHomePage());
+    router.get("/webhook", homepageController.getWebhook());
+    router.post("/webhook", homepageController.postWebhook());
+    router.post("/set-up-profile", homepageController.handleSetupProfile());
+    router.get("/set-up-profile", homepageController.getSetupProfilePage());
+    router.get("/info-order", homepageController.getInfoOrderPage());
+    router.post("/set-info-order", homepageController.setInfoOrder());
     return app.use("/", router);
 };
 
