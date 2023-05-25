56cd4bc00d4e7efba704748c442f0d4e3a61bb17
GIA CUONG <tgiacuong@gmail.com>
2023-05-25 17:19:04 +0900

    fix

--- src/services/homepageService.js
+++ src/services/homepageService.js
@@ -48,7 +48,7 @@ let handleSetupProfileAPI = () => {
                     }
                 ],
                 "whitelisted_domains": [
-                    "https://tech-shop-haryphamdev.herokuapp.com/"
+                    "https://chatbotshop.herokuapp.com/"
                 ]
             };
             // Send the HTTP request to the Messenger Platform
